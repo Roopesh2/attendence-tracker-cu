e8da3cf199244c3900ec2c4f5833885fdbab117f
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-05-08 11:51:57 +0530

    Added login manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './App.css'
 import { FormView } from './form'
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-
+	const [isLogged, _setLoginState] = useState(checkLoginFromCache());
+	function setLoginState(state) {
+		_setLoginState(state);
+		localStorage.setItem("isLogged", state)
+	}
 	return (
 		<>
-			<FormView />
+			{isLogged ? <p>Hello</p>: <FormView setLoginState={setLoginState}/>}
 		</>
 	)
 }
 
+function checkLoginFromCache() {
+	return localStorage.getItem("isLogged");
+}
 export default App
