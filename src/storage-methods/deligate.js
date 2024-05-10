eec4ff0267aff90843b7eae3e839d54d6e93cd16
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-05-10 13:22:05 +0530

    added storage manager functions

--- src/storage-methods/deligate.js
+++ src/storage-methods/deligate.js
@@ -1,3 +1,50 @@
-function StorageManager() {
+const ARRAY_SEPARATOR = "::";
+/** 
+ * localStorage Schema
+ * isLogged <bool> : whether logged in or not
+ * email <String>
+ * password <String>
+ * subjects <Array> : list of entered subjects
+ * timetable <Object<String, Array>> : 2d array of table
+ * 
+*/
+
+const StorageManager = {
+  isLoggedIn: () => {
+    localStorage.getItem("isLogged");
+  },
+  logOut: () => {
+    localStorage.setItem("isLogged", false);
+  },
+  setLoggedIn: () => {
+    localStorage.setItem("isLogged", true);
+  },
+
+  /**
+   * stores array of subjects as string separated by ::
+   * @param {Array<String>} subjects List of subjects
+   */
+  setSubjects: (subjects) => {
+		localStorage.setItem("subjects", JSON.stringify(subjects));
+  },
+
+  /**
+	 * Returns subjects entered.
+   * @returns {Array<String>}
+   */
+  getSubjects: () => {
+    return JSON.parse(localStorage.getItem("subjects"));
+  },
 	
-}
+	setTimeTable: (table) => {
+		localStorage.setItem("timetable", JSON.stringify(subjects));
+	},
+	
+	setTimeTable: (table) => {
+		localStorage.setItem("timetable", JSON.stringify(subjects));
+	}
+};
+
+StorageManager.setSubjects([1, 3, "ds: S"]);
+console.log(StorageManager.getSubjects());
+export default StorageManager;
