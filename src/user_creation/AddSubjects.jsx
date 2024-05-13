7689f78e1e768c92e5995fc262248fbf3160b435
Aswathsankar <aswathmenon111@gmail.com>
2024-05-13 10:51:54 +0530

    added the form

--- src/user_creation/AddSubjects.jsx
+++ src/user_creation/AddSubjects.jsx
@@ -1,4 +1,30 @@
+//import Subjectfield from "../Subjectfield";
+
+//function AddSubjects ()  {
+
+	//const [components, setComponents] = useState([{}]);
+	//const handleAdd = () => {
+	  //setComponents(prevComponents => [...prevComponents, {}]);
+	//};
+	//const handleDelete = (index) => {
+// 	  if (components.length > 1) {
+// 		setComponents(prevComponents => prevComponents.filter((component, i) => i !== index));
+// 	  }
+// 	};
+	
+// 	return (
+// 	  <div>
+// 		{components.map((component, index) => (
+// 		  <Subjectfield key={index} onDelete={() => handleDelete(index)} />
+// 		))}
+// 		<Button onClick={handleAdd}>Add Component</Button>
+// 	  </div>
+//   ); 
+// }
+// export default AddSubjects;
+import { Button } from "react-bootstrap";
 import Subjectfield from "../Subjectfield";
+import { useState } from "react";
 
 function AddSubjects ()  {
 
@@ -13,11 +39,15 @@ function AddSubjects ()  {
 	};
 	
 	return (
-	  <div>
-		{components.map((component, index) => (
-		  <Subjectfield key={index} onDelete={() => handleDelete(index)} />
-		))}
-		<Button onClick={handleAdd}>Add Component</Button>
+	  <div style={{display: 'flex', justifyContent: 'center'}}>
+		<form style={{border: '1px solid black', padding: '20px'}}>
+		  <h2 style={{textAlign: 'center'}}>Add Subjects</h2>
+		  {components.map((component, index) => (
+			<Subjectfield key={index} onDelete={() => handleDelete(index)} />
+		  ))}
+		  <Button onClick={handleAdd}>Add Subject</Button>
+		  <Button variant="contained" color="primary" style={{marginTop: '20px'}}>Next</Button>
+		</form>
 	  </div>
   ); 
 }
