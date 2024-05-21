2a6eab0dfcf20fad4da4192523fc53776f2f6ba3
Aswathsankar <aswathmenon111@gmail.com>
2024-05-21 13:17:34 +0530

    changed logo

--- src/login_screen.jsx
+++ src/login_screen.jsx
@@ -5,8 +5,8 @@ export function LoginScreen({ setLoginState }) {
   return (
     <div className="center-container">
       <div className="left">
-        <h1>Welcome</h1>
-        <img src="../public/logo-2.png" width="400px" />
+        <h1 style={{fontSize:"50px",marginBottom:"1rem",fontStyle:"italic"}}>Welcome</h1>
+        <img src="../public/logo-2.png" width="300px" />
       </div>
       <div className="right">
         <h1 className="name">Track Well</h1>
